cc9c1dd65d2cf7e2e5cad72d24767988cad0e896
shtabesh02 <shtabesh02@gmail.com>
2023-10-21 15:01:40 -0400

    new cols added

--- backend/db/seeders/20231019212909-demo-user.js
+++ backend/db/seeders/20231019212909-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'Mohammadjan',
+        lastName: 'Farasu',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Alisins',
+        lastName: 'Danesh',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Sharif',
+        lastName: 'Rezaie',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
