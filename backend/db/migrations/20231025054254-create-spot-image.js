ac86c5eb2eb7bb755ff5c33840f37769c15512c9
shtabesh02 <shtabesh02@gmail.com>
2023-10-26 18:28:24 -0400

    db changed

--- backend/db/migrations/20231025054254-create-spot-image.js
+++ backend/db/migrations/20231025054254-create-spot-image.js
@@ -1,4 +1,10 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -30,7 +36,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('spotImages');
