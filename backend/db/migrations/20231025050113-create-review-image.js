d15b9828e2216e0530f7a9a5d203c6e6e28e060d
shtabesh02 <shtabesh02@gmail.com>
2023-10-26 19:17:17 -0400

    spot created

--- backend/db/migrations/20231025050113-create-review-image.js
+++ backend/db/migrations/20231025050113-create-review-image.js
@@ -1,8 +1,14 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('reviewImages', {
+    await queryInterface.createTable('ReviewImages', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -12,7 +18,7 @@ module.exports = {
       reviewId: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'reviews'
+          model: 'Reviews'
         }
       },
       url: {
@@ -26,9 +32,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('reviewImages');
+    options.tableName = 'ReviewImages';
+    return queryInterface.dropTable('ReviewImages');
   }
 };
--- backend/db/migrations/20231026230641-create-spot.js
+++ backend/db/migrations/20231026230641-create-spot.js
@@ -1,8 +1,13 @@
+
 'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('spots', {
+    await queryInterface.createTable('Spots', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -50,9 +55,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('spots');
+    options.tableName = 'Spots';
+    return queryInterface.dropTable('Spots');
   }
 };
