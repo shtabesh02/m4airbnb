e362c4b63cd72dd063ca2c2da1cb7cae0ecaaaf4
shtabesh02 <shtabesh02@gmail.com>
2023-11-06 21:17:59 -0500

    alias added

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,9 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Spot.belongsTo(models.User, 
-        // {as: 'Owner'},
-         {
+      Spot.belongsTo(models.User, {as: 'Owner'}, {
         foreignKey: 'ownerId',
         // onDelete: 'cascade'
       });
