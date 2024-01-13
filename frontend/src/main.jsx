4de508daccb679037a570a7e08ad6d3a562dbd60
shtabesh02 <shtabesh02@gmail.com>
2024-01-12 23:56:48 -0500

    phase 0 done

---
+++ frontend/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import './index.css';
+import configureStore from './store';
+
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+const store = configureStore();
+
+// edited section
+// if (process.env.NODE_ENV !== 'production') {
+//   window.store = store;
+// }
+// edited section ends
+
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
