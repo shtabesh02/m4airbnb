cfab5537bfd7314b71de19bf1a2396cf289de1a8
shtabesh02 <shtabesh02@gmail.com>
2024-01-13 00:57:10 -0500

    Login - Session actions and reducer done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import configureStore from './store';
+import * as sessionActions from './store/session';
 
 import { restoreCSRF, csrfFetch } from './store/csrf';
 
@@ -21,6 +22,7 @@ if (import.meta.env.MODE !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 
