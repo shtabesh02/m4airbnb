d62caa18f077152b0176c1377eee00f318725aa8
shtabesh02 <shtabesh02@gmail.com>
2024-01-17 20:46:35 -0500

    landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <h1>Welcome!</h1>
+        element: <h1>Welcome to Nova bnb!</h1>
       }
     ]
   }
