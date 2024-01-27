29330df4673281439ee6930235b1b96c52572844
shtabesh02 <shtabesh02@gmail.com>
2024-01-27 16:38:05 -0500

    seeders updated

--- backend/db/seeders/20231027002718-demo-spotImage.js
+++ backend/db/seeders/20231027002718-demo-spotImage.js
@@ -58,7 +58,7 @@ module.exports = {
       preview: true
     },
     {
-      spotId: 5,
+      spotId: 6,
       url: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       preview: true
     }
