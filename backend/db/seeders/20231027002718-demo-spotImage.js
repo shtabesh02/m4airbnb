9780133c70527fb49ae90bc866956a3cb2f01b48
shtabesh02 <shtabesh02@gmail.com>
2024-01-27 17:51:13 -0500

    seeds

--- backend/db/seeders/20231027002718-demo-spotImage.js
+++ backend/db/seeders/20231027002718-demo-spotImage.js
@@ -69,7 +69,7 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1,2] }
+      spotId: { [Op.in]: [1, 2, 3, 4, 5, 6] }
     }, {});
   }
 };
