d272379bc72dec242aec966bae1bc07dbcd23e4a
shtabesh02 <shtabesh02@gmail.com>
2024-01-27 18:24:26 -0500

    spots

--- backend/db/seeders/20231027002718-demo-spotImage.js
+++ backend/db/seeders/20231027002718-demo-spotImage.js
@@ -41,26 +41,6 @@ module.exports = {
       spotId: 2,
       url: 'https://images.unsplash.com/photo-1505691723518-36a5ac3be353?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       preview: true
-    },
-    {
-      spotId: 3,
-      url: 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=2948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      preview: true
-    },
-    {
-      spotId: 4,
-      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1705772111/samples/landscapes/architecture-signs.jpg',
-      preview: true
-    },
-    {
-      spotId: 5,
-      url: 'https://images.unsplash.com/photo-1505691723518-36a5ac3be353?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      preview: true
-    },
-    {
-      spotId: 6,
-      url: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      preview: true
     }
    ]);
   },
@@ -69,7 +49,7 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1, 2, 3, 4, 5, 6] }
+      spotId: { [Op.in]: [1, 2] }
     }, {});
   }
 };
