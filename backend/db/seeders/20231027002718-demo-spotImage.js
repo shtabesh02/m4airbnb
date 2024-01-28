cd35df91f3eddac75e20fb4c020573d5081dba2c
shtabesh02 <shtabesh02@gmail.com>
2024-01-27 19:15:31 -0500

    new images

--- backend/db/seeders/20231027002718-demo-spotImage.js
+++ backend/db/seeders/20231027002718-demo-spotImage.js
@@ -14,52 +14,52 @@ module.exports = {
    await SpotImage.bulkCreate([
     {
       spotId: 1,
-      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1705772111/samples/landscapes/architecture-signs.jpg',
+      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1706400585/todd-kent-178j8tJrNlc-unsplash_epodhv.jpg',
       preview: true
     },
     {
       spotId: 1,
-      url: 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=2948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1706400583/scott-webb-1ddol8rgUH8-unsplash_1_gxuifg.jpg',
       preview: true
     },
     {
       spotId: 1,
-      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1705772111/samples/landscapes/architecture-signs.jpg',
+      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1706400583/rowan-heuvel-bjej8BY1JYQ-unsplash_1_npjupt.jpg',
       preview: true
     },
     {
       spotId: 1,
-      url: 'https://images.unsplash.com/photo-1505691723518-36a5ac3be353?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1706400583/spacejoy-YI2YkyaREHk-unsplash_bi3tkm.jpg',
       preview: true
     },
     {
       spotId: 1,
-      url: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1706400583/stephan-bechert-yFV39g6AZ5o-unsplash_hsjqew.jpg',
       preview: true
     },
     {
       spotId: 2,
-      url: 'https://images.unsplash.com/photo-1505691723518-36a5ac3be353?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1706400579/frames-for-your-heart-2d4lAQAlbDA-unsplash_x8g2aj.jpg',
       preview: true
     },
     {
       spotId: 3,
-      url: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1706400578/douglas-sheppard-9rYfG8sWRVo-unsplash_qeua8c.jpg',
       preview: true
     },
     {
       spotId: 4,
-      url: 'https://images.unsplash.com/photo-1505691723518-36a5ac3be353?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1706400578/brian-babb-XbwHrt87mQ0-unsplash_uux0yu.jpg',
       preview: true
     },
     {
       spotId: 5,
-      url: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1706400577/alexandra-gorn-JIUjvqe2ZHg-unsplash_vkqwst.jpg',
       preview: true
     },
     {
       spotId: 6,
-      url: 'https://images.unsplash.com/photo-1505691723518-36a5ac3be353?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      url: 'https://res.cloudinary.com/dqvwvomz1/image/upload/v1706400576/alberto-castillo-q-mx4mSkK9zeo-unsplash_flcrgd.jpg',
       preview: true
     }
    ]);
